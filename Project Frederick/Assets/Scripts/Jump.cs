eb809a9acf17ab2bc988d11a0e46df6017449d6d
demircankaragoz <demircankaragoz.tur@gmail.com>
2023-08-17 16:13:30 +0300

    a

--- Project Frederick/Assets/Scripts/Jump.cs
+++ Project Frederick/Assets/Scripts/Jump.cs
@@ -6,14 +6,13 @@ public class Jump : MonoBehaviour
 {
     [Range(1,10)]
     public float jumpVelocity;
-    
-   
     void Update()
     {
-        if (Input.GetButtonDown ("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
-
         }
     }
+
 }
+
