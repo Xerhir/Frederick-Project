b6e30a65c8733db28af85e3a8f0bcff4c91c3342
demircankaragoz <demircankaragoz.tur@gmail.com>
2023-08-18 01:47:03 +0300

    ziplama isi halledildi

--- Project Frederick/Assets/Scripts/Character Scripts/Jump.cs
+++ Project Frederick/Assets/Scripts/Character Scripts/Jump.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Jump : MonoBehaviour
 {
     public float jumpForce = 5f;
     public LayerMask groundLayer;
